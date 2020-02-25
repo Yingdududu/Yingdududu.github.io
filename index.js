804b5cf5c811a42c361eb8803c89d36ef26a57cb
Ying Du <19duying97@gmail.com>
2020-02-24 21:17:39 -0500

    cu landmark clf

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 CLASSES = {
-  0: 'class0',
-  1: 'class1',
-  2: 'class2',
-  3: 'class3',
-  4: 'class4',
+  0: 'Alma Matter sculpture',
+  1: 'Curl sculpture',
+  2: 'Lion sculpture',
 };
 
 const MODEL_PATH =
     'model.json';
 
-const IMAGE_SIZE = 192;
-const TOPK_PREDICTIONS = 5;
+const IMAGE_SIZE = 150;
+const TOPK_PREDICTIONS = 3;
 
 let my_model;
 const demo = async () => {
